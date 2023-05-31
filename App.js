3455013e9964b755cbb023395d13d8465d22d3ee
wbkong <hmy1219@naver.com>
2023-05-31 10:28:46 +0900

    버튼 클릭 이벤트 처리 예제문 1. 텍스트 변경 2. 다중 변경 3. true / false 변경 4. 카운트 되는것

--- App.js
+++ App.js
@@ -1,33 +1,69 @@
-/**
- * @format
- */
-
-import React from 'react';
+import React , {Component, useState}from 'react'; 
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Alert,
+  Button,
+  Linking,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-const App = () => {
-  return (
-    <View>
-      <Text style={styles.text}>HELLOOOOOOOOOOOO</Text>  
+export default function App() {
+  // const name = 'WooBin';
+
+  const [count , setCount] = useState(0);
+
+  const [current , setCurrent] = useState(true);
+  const [name, setName] = useState('Koban');
+  const [age, setAge] = useState({    
+    number : 19,    
+    title : 'GIMEIMG'  
+  });
+
+  function ButtonClickHandler() {
+    setCount(count + 1);
+
+    // name = 'asdasd';
+    // Alert.alert('qwe');
+    // setName('WooBin!!!!!!!!!!!!!!');
+    // console.log("TEST!!!!!!!!!!!!!!"); 
+    // // android 에서 로그처럼 확인할수있음 이제 저 로그는 Consol 에서만 확인이 가능함
+    // setAge({
+    //   number : 28,
+    //   title : 'omg'
+    // })
+    // setCurrent(false);
+  }
+
+  return(
+    <View style={styles.body}>
+      {/* <Text style={styles.text}>My name is {name}</Text>
+      <Text style={styles.text}>This is age {age.number} and {age.title}</Text>
+      <Text style={styles.text}>{current ? 'true' : 'false'}</Text> */}
+      <Text style={styles.text}>Count(5) {count * 5}</Text>
+      <Button title='UP Count' onPress={ButtonClickHandler}></Button>
+      <Text style={styles.text}>Count(1) {count}</Text>
     </View>
-  );
-  
+  )
+
 }
+// 위처럼 컴포넌트 사용
 
 const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#6799FF',
+    alignItems:'center',
+    justifyContent:'center',
+  },
   text : {
-    color : 'black',
-    fontSize : 30,
+    color : '#FFFFFF',
+    fontSize : 24,
+    fontStyle:'italic',
+    margin : 24,
   },
 
 });
 
-export default App;
+// export default App
