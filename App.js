7bc62566c0896f48758b601bc1de1a1c6494ab17
wbkong <hmy1219@naver.com>
2023-06-02 13:12:19 +0900

    FlatList 사용

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 import {
   Button,
+  FlatList,
   RefreshControl,
   ScrollView,
   StyleSheet,
@@ -10,28 +11,43 @@ import {
 // 코드 자동 정렬 : Shift + Alt + F
 const App = () => {
 
-const a = 0;
+  const a = 0;
 
   const [Items, setItems] = useState([
-    { key: 1, Items: 'Items 1' },
-    { key: 2, Items: 'Items 2' },
-    { key: 3, Items: 'Items 3' },
-    { key: 4, Items: 'Items 4' },
-    { key: 5, Items: 'Items 5' },
-    { key: 6, Items: 'Items 6' },
-    { key: 7, Items: 'Items 7' },
-    { key: 8, Items: 'Items 8' },
-    { key: 9, Items: 'Items 9' },
-    { key: 10, Items: 'Items 10' },
-    { key: 11, Items: 'Items 11' },
+    { name: 'Items 1' },
+    { name: 'Items 2' },
+    { name: 'Items 3' },
+    { name: 'Items 4' },
+    { name: 'Items 5' },
+    { name: 'Items 6' },
+    { name: 'Items 7' },
+    { name: 'Items 8' },
+    { name: 'Items 9' },
+    { name: 'Items 10' },
+    { name: 'Items 11' },
   ]);
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1', 'Item 3-2'],
+    },
+
+  ]
 
   const [Ref, setRef] = useState(false);
 
   const onRefresh = () => {
     setRef(true);
     // "..." 배열끝에 붙히기 세개의 점
-    setItems([...Items, { key: a + 1, Items: 'Items 12' }]);
+    setItems([...Items, { name: 'Items 123' }]);
     console.log(a);
     setRef(false);
   }
@@ -40,30 +56,52 @@ const a = 0;
   // jsx 는 view 안에 js 를 사용 가능 map() 안에 함수 아무거나 써도댐
   // ScrollView 사용 할 떄 View 자체를 빼도 댐
   return (
-    // <View style={styles.body}>
-    <ScrollView
-      style={styles.body}
+
+    // horizontal 가로 스크롤이 되게 하는거임 
+    // inverted 역으로 정리 해주는거임
+    <FlatList
+      // horizontal
+      // inverted
+      // numColumns={2} // 표시하는 개수 기본 1개 
+      keyExtractor={(item, index) => index.toString()}
+      data={Items}
+      // 괄호 -> 중괄호 안에 넣어야함 ({item}) 함수는 무조건 item 인듯 ㅇㅇ 
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
       refreshControl={
         <RefreshControl
           refreshing={Ref}
           onRefresh={onRefresh}
-          colors={['#ffffaa']}
-
+          colors={['red']}
         />
       }
+    />
+    // <View style={styles.body}>
+    // <ScrollView
+    //   style={styles.body}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={Ref}
+    //       onRefresh={onRefresh}
+    //       colors={['#ffffaa']}
+    //     />
+    //   }
 
-    >
+    // >
 
-      {
-        Items.map((iiii) => {
-          return (
-            <View style={styles.item} key={iiii.key}>
-              <Text style={styles.text}>{iiii.Items}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView>
+    //   {
+    //     Items.map((iiii) => {
+    //       return (
+    //         <View style={styles.item} key={iiii.key}>
+    //           <Text style={styles.text}>{iiii.Items}</Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    // </ScrollView>
     // </View>
   )
 
